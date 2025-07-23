9175666ae4dfccd760ff4e097a7a3a1d5512cbde
Yunoya <sonrasamon15725@gmail.com>
2025-07-23 20:44:48 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { cors } from "hono/cors";
 import apiRouter from "./routes/api.js";
 import { handle } from "hono/vercel";
+import { serve } from "@hono/node-server";
 
 const app = new Hono().basePath("/api");
 
@@ -25,3 +26,13 @@ export const config = {
 };
 
 export default handle(app);
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
