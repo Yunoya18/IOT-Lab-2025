e61101dfa88dc11a3038ebade7a18964e0721bfe
Yunoya <sonrasamon15725@gmail.com>
2025-07-30 23:17:07 +0700

    Update drizzle.ts
    ลืมเอา dotenv ออก

--- api/db/drizzle.ts
+++ api/db/drizzle.ts
@@ -1,7 +1,5 @@
 import { drizzle as drizzlePgsql } from "drizzle-orm/vercel-postgres";
 import * as schema from "./schema.js";
-import dotenv from "dotenv";
-dotenv.config();
 
 const drizzle = drizzlePgsql({
   casing: "snake_case",
