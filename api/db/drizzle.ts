b7b74b091170cc204878bc6d168bebe03b0f2111
Yunoya <sonrasamon15725@gmail.com>
2025-07-30 23:15:06 +0700

    edit table
    นิดนึงคับพี่ แหะๆ

--- api/db/drizzle.ts
+++ api/db/drizzle.ts
@@ -1,5 +1,7 @@
 import { drizzle as drizzlePgsql } from "drizzle-orm/vercel-postgres";
 import * as schema from "./schema.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const drizzle = drizzlePgsql({
   casing: "snake_case",
--- api/db/schema.ts
+++ api/db/schema.ts
@@ -3,7 +3,7 @@ import * as t from "drizzle-orm/pg-core";
 export const students = t.pgTable("students", {
   id: t.serial("id").primaryKey(),
   name: t.text("name").notNull(),
-  surname: t.text("name").notNull(),
+  surname: t.text("surname").notNull(),
   dob: t.date("dob").notNull(),
   gender: t.text("gender").notNull()
 });
